3673c07705e255b38dd3cc016aa277b3fd77ca21
陆柏宏 <wanqingsi1438438@gmail.com>
2022-08-14 22:15:20 +0800

    等cnx看看那个self.user.scheduleTasks()问题在哪

--- src/frontend/dispatch.py
+++ src/frontend/dispatch.py
@@ -1,1 +1,93 @@
 # 点击工具栏的调度按钮所显示的页面
+from src.backend.method import *
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import QDate, QDateTime, QTime
+from PyQt5.QtGui import QIcon, QFont, QPixmap
+from PyQt5.QtWidgets import qApp, QLabel, QLineEdit, QPushButton, \
+    QGridLayout, QVBoxLayout, QHBoxLayout, QApplication, QDesktopWidget, \
+    QWidget, QMessageBox, QInputDialog, QMainWindow, QCalendarWidget, QFormLayout, QDateTimeEdit, QTimeEdit, QTextEdit, \
+    QGroupBox, QScrollArea
+
+from src.frontend import addTask
+from src.frontend.TaskLabel import DailyTaskLabel, NormalTaskLabel
+
+
+class Dispatcher(QWidget):
+    def __init__(self, user, calenWindow):
+        super(Dispatcher, self).__init__()
+        self.scroll = QScrollArea()
+        self.user = user
+        self.calenWindow = calenWindow
+        self.displayingTasks = None
+        self.initUI()
+
+    def initUI(self):
+        self.formLayout = QFormLayout()
+        self.groupBox = QGroupBox()
+
+        self.displayingTasks = self.user.scheduleTasks()
+        self.taskNum = len(self.displayingTasks)
+
+        if self.taskNum > 0:
+            widget = QLabel("今日所有待办的调度安排如下：")
+            font = QFont()
+            font.setPointSize(12)
+            font.setBold(True)
+            # font.setFamily("KaiTi")
+            widget.setFont(font)
+            self.formLayout.addRow(widget)
+
+            titleFont = QFont()
+            titleFont.setBold(True)
+            titleWidget = QWidget()
+            hbox = QHBoxLayout()
+            sortLabel = QLabel('类别')
+            stateLabel = QLabel('状态')
+            nameLabel = QLabel('名称')
+            timeLabel = QLabel('时间')
+            sortLabel.setFont(titleFont)
+            stateLabel.setFont(titleFont)
+            nameLabel.setFont(titleFont)
+            timeLabel.setFont(titleFont)
+
+            hbox.addWidget(sortLabel)
+            hbox.addWidget(stateLabel)
+            hbox.addWidget(nameLabel)
+            hbox.addWidget(timeLabel)
+            for i in range(4):
+                hbox.addWidget(QLabel())
+            titleWidget.setLayout(hbox)
+            self.formLayout.addRow(titleWidget)
+
+            for task in self.displayingTasks:
+                widget = self.generateTaskWidget(task)
+                self.formLayout.addRow(widget)
+            self.groupBox.setLayout(self.formLayout)
+        else:
+            label = QLabel("今日暂无待办，不需要进行调度哦～")
+            font = QFont()
+            font.setPointSize(16)
+            font.setBold(True)
+            # font.setFamily("KaiTi")
+            label.setFont(font)
+
+            self.formLayout.addWidget(label)
+            self.formLayout.addWidget(QLabel())
+            self.groupBox.setLayout(self.formLayout)
+
+        # self.disableHorizontalScroll()
+        self.scroll.setWidget(self.groupBox)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setFixedHeight(400)  # 对应CalenWindow高度
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.scroll)
+        # self.show()
+
+    def generateTaskWidget(self, task: Task):
+        if isinstance(task, DailyTask):
+            taskLabel = DailyTaskLabel(date=task.time, task=task, user=self.user,
+                                       calenWindow=self.calenWindow)
+        else:
+            taskLabel = NormalTaskLabel(task=task, user=self.user, calenWindow=self.calenWindow)
+        return taskLabel
+
